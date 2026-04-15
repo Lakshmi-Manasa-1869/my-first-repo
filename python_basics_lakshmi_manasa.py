273a48ae2cd0045b1147bdae5f4648193afb695d
Lakshmi-Manasa-1869 <lukshmimannasah@gmail.com>
2026-04-15 23:50:24 +0530

    Add files via upload

---
+++ python_basics_lakshmi_manasa.py
@@ -0,0 +1,17 @@
+person_name = "Lakshmi Manasa"
+age = 26
+height = 4.11
+is_student = "yes"
+
+print(person_name, age, height, is_student)
+
+print("Name:", person_name, "|", "Age:", age, "|", "Height:", height, "|", "Student:", is_student)
+
+age_in_months = age * 12
+print("Age_in_months:",age_in_months)
+age_in_days = age * 365
+print("Age_in_days:",age_in_days)
+remainder_when_divided_by_7 = age % 7
+print("Remainder_when_divided_by_7:",remainder_when_divided_by_7)
+age_raised_to_power_of_2 = age ** 2
+print("Age_raised_to_power_of_2:",age_raised_to_power_of_2)
